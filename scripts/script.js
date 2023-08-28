8637b2454e25bbc94ad2a1d17798daba54e7f688
Tiago Queirós <tiagoqueiros@users.noreply.github.com>
2023-08-29 00:27:27 +0100

    feat: include attack flag
    * revert: smooth scroll
    * fix: skipped lc farm

--- scripts/script.js
+++ scripts/script.js
@@ -25,11 +25,13 @@ const setSleep = async (callback, timing = 2) => {
 
 const startFarm = () => {
   let _cmdCount = 0;
+  let _incomingCount = 0;
   let intervalLock = false;
   console.log("Taming started");
 
   const homeQueryParam = "screen=overview";
   const farmQueryParam = "screen=am_farm";
+  const incomingQueryParam = "mode=incomings";
 
   const intervalId = setInterval(async () => {
     if(!!document.querySelectorAll(".popup_box_close")[0]) {
@@ -48,7 +50,24 @@ const startFarm = () => {
       return false;
     }
 
-    if(document.location.search.includes(homeQueryParam)) {
+    if(document.querySelectorAll("#incomings_cell").length > 0
+        && parseInt(document.querySelectorAll("#incomings_cell")[0].innerText) > _incomingCount) {
+      console.log("Welcome customer");
+      _incomingCount = parseInt(document.querySelectorAll("#incomings_cell")[0].innerText)
+      await setSleep(() => document.querySelectorAll("#incomings_cell a")[0].click());
+    }
+
+    if(document.location.search.includes(incomingQueryParam)) {
+      intervalLock = true;
+      document.querySelectorAll("#select_all")[0].click();
+      document.querySelectorAll("input[value='Label']")[0].click();
+      console.log("Customer delivered");
+      await setSleep(() => document.querySelectorAll("#menu_row2_village a")[0].click());
+
+      intervalLock = false;
+    }
+
+    else if(document.location.search.includes(homeQueryParam)) {
       intervalLock = true;
 
       // 1. Grow the stable
@@ -94,10 +113,11 @@ const startFarm = () => {
         // 2.1. If enough LC, go to LA
         if (lcCount >= 4 && spyCount >= 1) {
           console.log("Going to tame the horses");
-          window.scrollTo({ top: 0, behavior: "smooth" });
           await setSleep(() => document.querySelectorAll("#manager_icon_farm")[0].click());
         }
       }
+
+      intervalLock = false;
     }
 
     else if (document.location.search.includes(farmQueryParam)) {
@@ -108,7 +128,6 @@ const startFarm = () => {
 
       if(spyCount < 1 || lightCount < 4) {
         console.log("Back to stable door");
-        window.scrollTo({ top: 0, behavior: "smooth" });
         await setSleep(() => document.querySelectorAll("#menu_row2_village a")[0].click());
         intervalLock = false;
         return false;
@@ -126,7 +145,7 @@ const startFarm = () => {
         const _dist = parseFloat(_vil.querySelectorAll("td")[7].innerText) * lcSpeed;
         let _res = 0;
 
-        _vil.querySelectorAll(".res").forEach(r => _res += parseInt(r.innerText));
+        _vil.querySelectorAll("td")[5].querySelectorAll(".nowrap").forEach(r => _res += parseInt(r.innerText.replace(".", "")));
         _res += _dist * (prodOverall * 3) / 60;
         _res += -_atkCount * lcCarry;
 
@@ -136,19 +155,16 @@ const startFarm = () => {
 
           if(spyCount < 1 || lightCount < 4) {
             console.log("Back to stable door");
-            window.scrollTo({ top: 0, behavior: "smooth" });
             await setSleep(() => document.querySelectorAll("#menu_row2_village a")[0].click());
             intervalLock = false;
             return false;
-          } else {
-            window.scrollTo({ top: _vil.getBoundingClientRect().top || 0, behavior: "smooth" });
-            await setSleep(() => _vil.querySelector(".farm_icon.farm_icon_a").click(), 5);
           }
+
+          await setSleep(() => _vil.querySelector(".farm_icon.farm_icon_a").click(), 5);
         }
       }
 
       console.log("Back to stable door");
-      window.scrollTo({ top: 0, behavior: "smooth" });
       await setSleep(() => document.querySelectorAll("#menu_row2_village a")[0].click());
 
       intervalLock = false;
